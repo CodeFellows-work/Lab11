9360f1c5a88069835d55b9055265b97578e66b56
sunny-lee3 <sunnylee3712@gmail.com>
2021-06-28 13:18:07 -0700

    lab 11 CAPS

--- __tests__/event-handler.test.js
+++ __tests__/event-handler.test.js
@@ -1,1 +1,31 @@
+'use strict'; 
 
+'use strict';
+const events =require('../events.js');
+
+
+describe('testing event handlers', ()=> {
+  let payload = {
+    store :'ABC Store',
+    orderID : '12345678910',
+    customer : 'Dwayne "The Rock" Johnson',
+    address : 'ABC Store at 12345 333th St SW',
+  };
+events.emit = jest.fn();
+// console.log = jest.fn();
+  it('test pickup', async ()=> {
+    events.emit('pickup',payload);
+    expect(events.emit).toHaveBeenCalled();
+  });
+
+  it('test in-transit', async ()=> {
+    events.emit('in-transit',payload);
+    expect(events.emit).toHaveBeenCalled();
+  });
+
+  it('test delivered', async ()=> {
+    events.emit('delivered',payload);
+    expect(events.emit).toHaveBeenCalled();
+  });
+    
+});
